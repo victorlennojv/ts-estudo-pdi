5f1cb266a881bb7a8f42ab216955d0704d3c4b8f
Victor Almeida <jpvvictor@hotmail.com>
2024-04-23 00:50:14 -0300

    First lesson - the basics of ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
-class Book {
-  constructor (public title: string) {
+class Person {
+  name: string;
+  age: number;
 
+  constructor (name: string, age: number) {
+    this.name = name;
+    this.age = age;
   }
 }
 
-let book: Book;
 
-book = new Book("test");
+const persons: Person[] = [];
+persons[0] = new Person("Resolvo", 12);
+persons[1] = { name: "Daniel", age: 4 };
+persons[2] = { name: "Collin" };
 
-console.log(book)
+// Inline type
+
+const personss: { name: string, age?: number }[] = []; 
+personss[0] = { name: "Ricardo", age: 2 };
+personss[1] = { name: "Daniel", age: 4 };
+personss[2] = { name: "Collin" };
+
+// Inline type [2]
+// type Name = string;
+// type Age = number;
+type Person2 = { name: string, age?: number }; // type Person2 = { name: Name, age: Age }
+const personsss: Person2[] = [];
+personsss[0] = { name: "Ricardo", age: 2 };
+personsss[1] = { name: "Daniel", age: 4 };
+personsss[2] = { name: "Collin" };
