6378f1f86e29e1f3930369a950eec41a94d64002
Victor Almeida <jpvvictor@hotmail.com>
2024-04-24 14:34:25 -0300

    Merge type

--- src/main.ts
+++ src/main.ts
@@ -29,3 +29,16 @@ const personsss: Person2[] = [];
 personsss[0] = { name: "Ricardo", age: 2 };
 personsss[1] = { name: "Daniel", age: 4 };
 personsss[2] = { name: "Collin" };
+
+// type merge
+
+type Entity = {
+  id: number;
+}
+
+type testMerge = Person & Entity;
+
+const person3: testMerge[] = [];
+
+person3[0] = { name: "Daniel Lucas", age: 4 }; // Becasuse of the merge between Person and Entity, this guy needs an id to accomplish the type.
+person3[1] = { name: "Daniel El", age: 1, id: 3 };
